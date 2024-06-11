94da44e26abb282c544eaa4449b1d14f1f7eea8a
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-06-11 12:48:49 +0900

    (feat) : springBoot 추가

--- src/main/java/org/example/demo/Main.java
+++ src/main/java/org/example/demo/Main.java
@@ -1,8 +1,12 @@
 package org.example.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
 }
