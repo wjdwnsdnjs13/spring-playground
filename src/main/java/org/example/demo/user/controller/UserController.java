489ca42b9efa3de47694943dc9153ba163a75447
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-06-12 17:02:05 +0900

    (feat) : 회원가입 구현중

--- src/main/java/org/example/demo/user/controller/UserController.java
+++ src/main/java/org/example/demo/user/controller/UserController.java
@@ -1,4 +1,25 @@
 package org.example.demo.user.controller;
 
+import org.example.demo.user.dto.UserRegisterRequestDto;
+import org.example.demo.user.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
+	private final UserService userService;
+
+	@PostMapping()
+	public ResponseEntity<?> userRegister(@RequestBody UserRegisterRequestDto userRegisterRequestDto){
+		userService.userRegister(userRegisterRequestDto);
+		return ResponseEntity.status(HttpStatus.OK).body("hi");
+	}
 }
