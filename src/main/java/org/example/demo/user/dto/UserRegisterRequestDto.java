9fea0662f6145f9177c4fb5747438e01d37202ea
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-06-12 17:02:12 +0900

    (feat) : 회원가입 구현중

--- src/main/java/org/example/demo/user/dto/UserRegisterRequestDto.java
+++ src/main/java/org/example/demo/user/dto/UserRegisterRequestDto.java
@@ -1,4 +1,8 @@
 package org.example.demo.user.dto;
 
+import lombok.Getter;
+
+@Getter
 public class UserRegisterRequestDto {
+	private String name;
 }
--- src/main/java/org/example/demo/user/service/UserService.java
+++ src/main/java/org/example/demo/user/service/UserService.java
@@ -1,4 +1,12 @@
 package org.example.demo.user.service;
 
+import org.example.demo.user.dto.UserRegisterRequestDto;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+	public boolean userRegister(UserRegisterRequestDto userRegisterRequestDto){
+
+	}
 }
