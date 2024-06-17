27a6e72cf2a76293b15235663c83cae04dc98897
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-06-17 23:57:25 +0900

    (feat) : 회원가입 요청 DTO - Entity 변환 구현

--- src/main/java/org/example/demo/user/dto/UserRegisterRequestDto.java
+++ src/main/java/org/example/demo/user/dto/UserRegisterRequestDto.java
@@ -1,8 +1,15 @@
 package org.example.demo.user.dto;
 
+import org.example.demo.user.entity.User;
+
 import lombok.Getter;
 
 @Getter
 public class UserRegisterRequestDto {
 	private String name;
+
+	public User toUser(){
+		return User.builder()
+			.name(this.name).build();
+	}
 }
