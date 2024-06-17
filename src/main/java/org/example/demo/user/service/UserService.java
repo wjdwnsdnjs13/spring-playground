ad725cfb33c60894909f1943b09aaa29d37c755e
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-06-17 23:59:11 +0900

    (feat) : 회원가입 구현

--- src/main/java/org/example/demo/user/service/UserService.java
+++ src/main/java/org/example/demo/user/service/UserService.java
@@ -1,12 +1,20 @@
 package org.example.demo.user.service;
 
 import org.example.demo.user.dto.UserRegisterRequestDto;
+import org.example.demo.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-	public boolean userRegister(UserRegisterRequestDto userRegisterRequestDto){
+	private final UserRepository userRepository;
 
+	public boolean userRegister(UserRegisterRequestDto userRegisterRequestDto){
+		if(userRepository.findByName(userRegisterRequestDto.getName()) != null) return false;
+		userRepository.save(userRegisterRequestDto.toUser());
+		return true;
 	}
 }
