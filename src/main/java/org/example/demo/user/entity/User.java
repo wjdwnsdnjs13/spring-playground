34e32f72952b87e10c6b153ed55b0128ddcb3a5b
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-06-17 23:56:36 +0900

    (feat) : User Entity 구현

---
+++ src/main/java/org/example/demo/user/entity/User.java
@@ -0,0 +1,20 @@
+package org.example.demo.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "user")
+public class User {
+	@Id
+	String name;
+}
